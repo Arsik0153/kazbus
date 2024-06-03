236d1921a068017069536c773556a59cd9c2fcfc
farayvl <fara.ot.ferrari@gmail.com>
2024-06-03 20:07:50 +0500

    added trip card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import FullPageSelector from '@/components/fullpageselector';
 import ArrowRightIcon from '../../public/assets/arrow-right-icon';
-
+import Counter from '@/components/counter';
+import Trip from '@/components/trip';
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between bg-sky-500/100 p-10">
@@ -9,6 +10,8 @@ export default function Home() {
                 icon={<ArrowRightIcon />}
                 text={'Откуда вы направляетесь?'}
             />
+            <Counter/>
+            <Trip/>
         </main>
     );
 }
