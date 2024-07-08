568cb2cf8594e9c1edd25a8bf3c07c448c301c9f
Zhanarys <arsik0153@gmail.com>
2024-07-09 02:36:01 +0500

    Global layout fixes

---
+++ src/app/(mobile)/layout.tsx
@@ -0,0 +1,14 @@
+import NavBar from '@/components/nav-bar';
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="flex min-h-screen items-stretch">
+            <div className="mb-[85px] flex-1">{children}</div>
+            <NavBar />
+        </div>
+    );
+}
--- src/app/(mobile)/main/page.tsx
+++ src/app/(mobile)/main/page.tsx
@@ -1,56 +1,55 @@
-import React from 'react'
+import React from 'react';
 import FullPageSelector from '@/components/fullpageselector';
 import ArrowLeftIcon from '../../../../public/assets/arrow-left-icon';
 import Button from '@/components/button';
-import NavBar from '@/components/nav-bar';
 import ArrowRightIcon from '../../../../public/assets/arrow-right-icon';
 import Calendar from '../../../../public/assets/calendar';
 import User from '../../../../public/assets/user';
 import Image from 'next/image';
 
 const MainPage = () => {
-  return (
-    <div className='bg-gradient-to-r from-[#E32828] to-[#E13535] flex flex-col h-screen flex-grow justify-center items-center'>
-        <div className='text-center flex items-center justify-center flex-col'>
-          <Image
-            src={'/assets/main/Ellipse-1.png'}
-            width={622}
-            height={750.49}
-            alt={''}
-            className="absolute h-screen w-screen"
-          />
-          <Image
-            src={'/assets/main/Ellipse-2.png'}
-            width={622}
-            height={750.49}
-            alt={''}
-            className="absolute bottom-0 left-0"
-          />
-          <div className='text-white text-[36px] font-medium tracking-[-3%] leading-[39.6px] mb-10 z-10'>
-            Поиск дешевых билетов на автобусы между городами
-          </div>
-          <div className='flex flex-col gap-2 z-10 w-screen p-5'>
-            <FullPageSelector
-              icon={<ArrowLeftIcon color="white" />}
-              text={'Откуда вы направляетесь?'}
-            />
-            <FullPageSelector
-              icon={<ArrowRightIcon color="white" />}
-              text={'Куда вы направляетесь?'}
-            />
-            <FullPageSelector
-              icon={<Calendar color="white" />}
-              text={'Дата отправления'}
-            />
-            <FullPageSelector
-              icon={<User color="white" />}
-              text={'1 пассажир'}
-            />
-            <Button>Начать поиск</Button>
-          </div>
+    return (
+        <div className="flex h-full flex-grow flex-col items-center justify-center bg-gradient-to-r from-[#E32828] to-[#E13535]">
+            <div className="flex flex-col items-center justify-center text-center">
+                <Image
+                    src={'/assets/main/Ellipse-1.png'}
+                    width={622}
+                    height={750.49}
+                    alt={''}
+                    className="absolute h-screen w-screen"
+                />
+                <Image
+                    src={'/assets/main/Ellipse-2.png'}
+                    width={622}
+                    height={750.49}
+                    alt={''}
+                    className="absolute bottom-0 left-0"
+                />
+                <div className="z-10 mb-10 px-5 text-[36px] font-medium leading-[39.6px] tracking-[-3%] text-white">
+                    Поиск дешевых билетов на автобусы между городами
+                </div>
+                <div className="z-10 flex w-screen flex-col gap-2 p-5">
+                    <FullPageSelector
+                        icon={<ArrowLeftIcon color="white" />}
+                        text={'Откуда вы направляетесь?'}
+                    />
+                    <FullPageSelector
+                        icon={<ArrowRightIcon color="white" />}
+                        text={'Куда вы направляетесь?'}
+                    />
+                    <FullPageSelector
+                        icon={<Calendar color="white" />}
+                        text={'Дата отправления'}
+                    />
+                    <FullPageSelector
+                        icon={<User color="white" />}
+                        text={'1 пассажир'}
+                    />
+                    <Button>Начать поиск</Button>
+                </div>
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default MainPage;  
+export default MainPage;
