79f3899f3e848dd7b2c81470907766a4185a03ab
Arsik0153 <arsik0153@gmail.com>
2024-08-10 18:32:28 +0500

    Fix combobox accessibility issues

--- src/app/admin/main/trips/_components/inputCombo.tsx
+++ src/app/admin/main/trips/_components/inputCombo.tsx
@@ -1,5 +1,10 @@
-import { Combobox, ComboboxInput, ComboboxOption, ComboboxOptions, ComboboxButton } from '@headlessui/react';
-import { CheckIcon } from '@heroicons/react/20/solid';
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxOption,
+    ComboboxOptions,
+    ComboboxButton,
+} from '@headlessui/react';
 import { useState } from 'react';
 import DownBtn from '@/assets/admin/DownBtb';
 
@@ -28,7 +33,9 @@ function ComboBox({ name, onSelectionChange = () => {} }: ComboBoxProps) {
     const filteredPeople =
         query === ''
             ? cruise
-            : cruise.filter((person) => person.name.toLowerCase().includes(query.toLowerCase()));
+            : cruise.filter((person) =>
+                  person.name.toLowerCase().includes(query.toLowerCase())
+              );
 
     return (
         <div className="relative">
@@ -43,27 +50,38 @@ function ComboBox({ name, onSelectionChange = () => {} }: ComboBoxProps) {
                 <div className="relative w-full">
                     <ComboboxInput
                         aria-label="Assignee"
-                        displayValue={(person: Person | null) => (person ? person.name : '')}
+                        displayValue={(person: Person | null) =>
+                            person ? person.name : ''
+                        }
                         onChange={(event) => setQuery(event.target.value)}
-                        className="border border-gray-300 rounded-[10px] p-3 pl-4 text-base font-medium text-[#4A4A4A] focus:outline-none"
+                        className="rounded-[10px] border border-gray-300 p-3 pl-4 text-base font-medium text-[#4A4A4A] focus:outline-none"
                     />
-                    <ComboboxButton className="absolute top-[30%] right-4 flex items-center p-2">
-                        <DownBtn color='#4A4A4A' className='p-2' />
+                    <ComboboxButton className="absolute right-4 top-[30%] flex items-center p-2">
+                        <DownBtn color="#4A4A4A" className="p-2" />
                     </ComboboxButton>
                 </div>
-                <ComboboxOptions className="absolute z-10 mt-2 w-full border border-[#eaf1f8] rounded-[10px] bg-[#F1F5F9] p-3 shadow-lg focus:outline-none">
-                    {filteredPeople.map((person) => (
-                        <ComboboxOption key={person.id} value={person} className="group flex flex-col gap-2 p-2 hover:bg-white hover:rounded-[5px]">
-                            {({ selected }) => (
-                                <>
-                                    <div className="flex flex-row justify-between text-base font-semibold text-[#4A4A4A]">
-                                        {person.name}
-                                        <CheckIcon className={`h-5 w-5 ${selected ? 'visible' : 'invisible'}`} />
-                                    </div>
-                                    <div className="w-full h-0 border"></div>
-                                </>
+                <ComboboxOptions className="absolute z-10 mt-2 w-full rounded-[10px] bg-[#F1F5F9] p-3">
+                    {filteredPeople.map((person, index) => (
+                        <>
+                            <ComboboxOption
+                                key={person.id}
+                                value={person}
+                                className="group flex flex-col data-[focus]:rounded-[5px] data-[focus]:bg-white"
+                            >
+                                {({ selected }) => (
+                                    <>
+                                        <div
+                                            className={`flex flex-row justify-between p-2 text-base font-medium text-[#4A4A4A] hover:rounded-[5px] hover:bg-white ${selected ? 'bg-white' : ''}`}
+                                        >
+                                            {person.name}
+                                        </div>
+                                    </>
+                                )}
+                            </ComboboxOption>
+                            {index !== filteredPeople.length - 1 && (
+                                <div className="my-2 h-[1px] w-full bg-[#CDCDCD]" />
                             )}
-                        </ComboboxOption>
+                        </>
                     ))}
                 </ComboboxOptions>
             </Combobox>
