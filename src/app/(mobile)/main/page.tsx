5bc2a440587839d8b4a7d04197425da5884eedcd
Vanckoe <adil210@mail.ru>
2024-08-11 15:10:42 +0500

    www

--- src/app/(mobile)/main/page.tsx
+++ src/app/(mobile)/main/page.tsx
@@ -7,9 +7,9 @@ import SelectArrival from './_components/select-arrival';
 import SelectDate from './_components/select-date';
 import { useRouter, useSearchParams } from 'next/navigation';
 import toast from 'react-hot-toast';
+import SelectPassengerCount from './_components/select-passenger-count';
 import Ellipse from '@/assets/Ellipse';
 import EllipseDown from '@/assets/Ellipse-down';
-import SelectPassengerCount from './_components/select-passenger-count';
 
 const MainPageSuspended = () => {
     const router = useRouter();
@@ -32,49 +32,28 @@ const MainPageSuspended = () => {
     };
 
     return (
-        <Drawer.Root shouldScaleBackground>
-            <div className="flex h-full flex-grow flex-col items-center justify-center bg-gradient-to-r from-[#E32828] to-[#E13535]">
-                <div className="flex flex-col items-center justify-center text-center">
-                    <Image
-                        src={'/assets/main/Ellipse-1.png'}
-                        width={622}
-                        height={750.49}
-                        alt={''}
-                        className="pointer-events-none absolute h-screen w-screen"
-                    />
-                    <Image
-                        src={'/assets/main/Ellipse-2.png'}
-                        width={622}
-                        height={750.49}
-                        alt={''}
-                        className="pointer-events-none absolute bottom-0 left-0"
-                    />
-                    <div className="mb-10 px-5 text-[36px] font-medium leading-[39.6px] tracking-[-3%] text-white">
-                        Поиск дешевых билетов на автобусы между городами
-                    </div>
-                    <div className="flex w-screen flex-col gap-2 p-5">
-                        <SelectDeparture />
-                        <SelectArrival />
-                        <SelectDate />
-                        <Drawer.Trigger>
-                            <Input
-                                label={`${passengerCountParam} ${getStringByNumber(
-                                    passengerCountParam,
-                                    ['пассажир', 'пассажира', 'пассажиров']
-                                )}`}
-                                id="passengers"
-                                variant="ghost"
-                                iconLeft={<User color="white" />}
-                            />
-                        </Drawer.Trigger>
-                        <PassengerDrawer />
-                        <Button onClick={handleSearchClick} className="mt-1">
-                            Начать поиск
-                        </Button>
-                    </div>
+        <div className="flex h-full flex-grow flex-col items-center justify-center bg-gradient-to-r from-[#E32828] to-[#E13535]">
+            <div className="flex flex-col items-center justify-center text-center">
+                <div className="pointer-events-none absolute h-screen top-0 right-0 w-screen z-0 flex justify-end">
+                    <Ellipse />
+                </div>
+                <div className="pointer-events-none absolute h-screen botton-0 left-0 w-screen z-0 flex items-end justify-end">
+                    <EllipseDown />
+                </div>
+                <div className="mb-10 px-5 text-[36px] font-medium leading-[39.6px] tracking-[-3%] text-white">
+                    Поиск дешевых билетов на автобусы между городами
+                </div>
+                <div className="flex w-screen flex-col gap-2 p-5">
+                    <SelectDeparture />
+                    <SelectArrival />
+                    <SelectDate />
+                    <SelectPassengerCount />
+                    <Button onClick={handleSearchClick} className="mt-1">
+                        Начать поиск
+                    </Button>
                 </div>
             </div>
-        </Drawer.Root>
+        </div>
     );
 };
 
