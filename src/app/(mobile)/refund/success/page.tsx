f92e1efdea1bf0be468e9a299ecdcb7a40c81a28
Vanckoe <adil210@mail.ru>
2024-08-16 04:46:39 +0500

    new pages refund

---
+++ src/app/(mobile)/refund/success/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Topbar from '@/components/topbar';
+import Yes from '@/assets/'
+
+const Success = () => {
+    return (
+        <>
+            <Topbar backHref="/main">
+                <h1 className="text-[20px] font-medium leading-[46.2px] tracking-[-3%] text-white">
+                    Оформить возврат
+                </h1>
+            </Topbar>
+            <div className="flex flex-col px-5 pt-5">
+
+            </div>
+        </>
+    )
+}
+
+export default Success;
