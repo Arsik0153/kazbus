5b86de5e14a7f841f8a625078965e34488f6b33a
Vanckoe <adil210@mail.ru>
2024-08-16 04:59:03 +0500

    fixed ultra last build

--- src/app/(mobile)/refund/success/page.tsx
+++ src/app/(mobile)/refund/success/page.tsx
@@ -7,6 +7,7 @@ const Success = () => {
             <Topbar backHref="/main">
                 <h1 className="text-[20px] font-medium leading-[46.2px] tracking-[-3%] text-white">
                     Оформить возврат
+                    azazaz
                 </h1>
             </Topbar>
             <div className="flex flex-col px-5 pt-5">
