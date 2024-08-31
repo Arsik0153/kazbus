370819367b645e687103e4d8eccb7b8dac8b4901
Arsik0153 <arsik0153@gmail.com>
2024-08-31 17:22:40 +0900

    Add passenger list

--- src/app/(mobile)/profile/authorized/passenger-data/page.tsx
+++ src/app/(mobile)/profile/authorized/passenger-data/page.tsx
@@ -1,32 +1,47 @@
+'use client';
 import React from 'react';
 import PassengerDataCard from '@/components/passenger-data-card';
 import Topbar from '@/components/topbar';
+import { useServerActionQuery } from '@/lib/server-action-hooks';
+import { getMyPassengersAction } from '@/app/(mobile)/main/tickets/actions';
+import PassengerCard from '@/components/passenger-card';
+import Spinner from '@/components/spinner';
 
 const PassengerDataPage = () => {
+    const { data: passengers, isPending: isPassengersPending } =
+        useServerActionQuery(getMyPassengersAction, {
+            input: undefined,
+            queryKey: ['passengers'],
+        });
+
+    if (isPassengersPending) {
+        return (
+            <>
+                <Topbar backHref="/profile">
+                    <div className="flex flex-col items-center">
+                        Данные моих пассажиров
+                    </div>
+                </Topbar>
+                <div className="mt-[42px] flex flex-col px-5">
+                    <div className="flex justify-center gap-3 py-5">
+                        <Spinner size="md" />
+                    </div>
+                </div>
+            </>
+        );
+    }
+
     return (
         <>
             <Topbar backHref="/profile">Данные моих пассажиров</Topbar>
             <div className="my-6 grid gap-4 px-5">
-                <PassengerDataCard
-                    name="Купертино Стив Джобсович"
-                    birth_date="12.12.2012"
-                    document_number="04040595289782932"
-                />
-                <PassengerDataCard
-                    name="Купертино Стив Джобсович"
-                    birth_date="12.12.2012"
-                    document_number="04040595289782932"
-                />
-                <PassengerDataCard
-                    name="Купертино Стив Джобсович"
-                    birth_date="12.12.2012"
-                    document_number="04040595289782932"
-                />
-                <PassengerDataCard
-                    name="Купертино Стив Джобсович"
-                    birth_date="12.12.2012"
-                    document_number="04040595289782932"
-                />
+                {passengers?.length !== 0 &&
+                    passengers?.map((selectedUser) => (
+                        <PassengerCard
+                            key={selectedUser.user_id}
+                            user={selectedUser}
+                        />
+                    ))}
             </div>
         </>
     );
